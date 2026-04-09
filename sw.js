9d2e2bed174ff8027775b147ac5d1d8680124613
Jeremy Lee <77534800+dotolee12@users.noreply.github.com>
2026-04-09 17:57:26 +0900

    Add files via upload
    코덱스로 수정

--- sw.js
+++ sw.js
@@ -1,41 +1,7 @@
-const CACHE_NAME = 'giloa-cache-v5';
-
-self.addEventListener('install', function(e) {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(function(cache) {
-      return cache.addAll([
-        './',
-        './index.html',
-        './script.js',
-        './style.css',
-        './manifest.json',
-        './sw.js',
-        'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
-        'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js',
-        'https://unpkg.com/leaflet.markercluster@1.5.3/dist/leaflet.markercluster.js',
-        'https://unpkg.com/leaflet.markercluster@1.5.3/dist/MarkerCluster.css',
-        'https://unpkg.com/leaflet.markercluster@1.5.3/dist/MarkerCluster.Default.css'
-      ]);
-    })
-  );
-  self.skipWaiting();
+self.addEventListener("install", event => {
+  event.waitUntil(self.skipWaiting());
 });
 
-self.addEventListener('activate', function(e) {
-  e.waitUntil(
-    caches.keys().then(function(keys) {
-      return Promise.all(
-        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
-      );
-    })
-  );
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', function(e) {
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
-    })
-  );
+self.addEventListener("activate", event => {
+  event.waitUntil(self.clients.claim());
 });
